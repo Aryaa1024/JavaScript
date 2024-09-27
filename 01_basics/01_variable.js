f7f3d5ce867d83bb8a112b74288b58cc499027b3
Aryaa1024 <aryanpal1024@gmail.com>
2024-09-27 10:27:55 +0530

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const id=1 //Used for declaring the constant value
+let name="Aryan Pal Dhangar" //Used to declare the variable
+var email="aryanpal1024@gmail.com" // Also declares the variable but not understand the Scope
+mobileNo="7880497004" //Also used to declare the variable but its a bad practice
+let status
+/*
+Recommeded not to use var keyword to declare the variable because if we change the variable value in the scope it will change it globally.
+So instead of it we use let keywords.
+*/
+console.log(id);
+console.table([id,name,email,mobileNo,status]);
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all JS code as Newer Version
+
+// alert('Hello World') // We are using NodeJs not using Browser JS
+
+/*
+Primitive Datatypes
+-------------------
+number= 2 power 53
+bigint
+string="Sting"
+boolean=true,false
+null = standalone value(Representation of Null Value)
+undefined= unassigned values
+symbol= unique
+
+*/
